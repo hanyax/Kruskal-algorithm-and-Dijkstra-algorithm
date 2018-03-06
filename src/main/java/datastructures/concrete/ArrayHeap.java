17311a8cbcece6679373670f0dc4ef0217974053
Shawn Xu <hanyax@cs.washington.edu>
2018-03-06 13:38:59 -0800

    arrayhip

--- src/main/java/datastructures/concrete/ArrayHeap.java
+++ src/main/java/datastructures/concrete/ArrayHeap.java
@@ -1,7 +1,6 @@
 package datastructures.concrete;
-
 import datastructures.interfaces.IPriorityQueue;
-import misc.exceptions.NotYetImplementedException;
+import misc.exceptions.EmptyContainerException;
 
 /**
  * See IPriorityQueue for details on what each method must do.
@@ -9,16 +8,17 @@ import misc.exceptions.NotYetImplementedException;
 public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
     // See spec: you must implement a implement a 4-heap.
     private static final int NUM_CHILDREN = 4;
-
     // You MUST use this field to store the contents of your heap.
     // You may NOT rename this field: we will be inspecting it within
     // our private tests.
+    
     private T[] heap;
-
+    private int size;
     // Feel free to add more fields and constants.
-
+    
     public ArrayHeap() {
-        throw new NotYetImplementedException();
+        heap = this.makeArrayOfT(10);
+        this.size = 0;
     }
 
     /**
@@ -28,32 +28,99 @@ public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
      * Note that each element in the array will initially be null.
      */
     @SuppressWarnings("unchecked")
-    private T[] makeArrayOfT(int size) {
+    private T[] makeArrayOfT(int arraySize) {
         // This helper method is basically the same one we gave you
         // in ArrayDictionary and ChainedHashDictionary.
         //
         // As before, you do not need to understand how this method
         // works, and should not modify it in any way.
-        return (T[]) (new Comparable[size]);
+        return (T[]) (new Comparable[arraySize]);
     }
 
     @Override
     public T removeMin() {
-        throw new NotYetImplementedException();
+    		if (size == 0) {
+    			throw new EmptyContainerException("queue is empty");
+    		}
+        T result = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        this.removeMinHelper(0);
+        return result;
+    }
+    
+    private void removeMinHelper(int index) {
+    		int minIndex = this.findMinChildren(index);
+    		if (minIndex != index) {
+    			T temp = heap[index];
+    			heap[index] = heap[minIndex];
+    			heap[minIndex] = temp;
+    			removeMinHelper(minIndex);
+    		}
+    }
+    
+    private int findMinChildren(int index) {
+    		int min = index;
+    		int i = 1;
+    		while ((NUM_CHILDREN * index +i) < size && i <= NUM_CHILDREN) {
+    			if (heap[NUM_CHILDREN * index + i].compareTo(heap[min]) < 0) {
+    				min = NUM_CHILDREN * index + i;
+    			}
+    			i++;
+    		}
+    		return min;
     }
 
     @Override
     public T peekMin() {
-        throw new NotYetImplementedException();
+    		if (size == 0) {
+			throw new EmptyContainerException("queue is empty");
+		}
+        return heap[0];
     }
 
     @Override
     public void insert(T item) {
-        throw new NotYetImplementedException();
+    		if (item == null) {
+    			throw new IllegalArgumentException("No null item allowed");
+    		}
+    		ensureCapacity();
+    		heap[size] = item;
+    		insertHelper(size);
+    		size++;
+    }
+    
+    private void insertHelper(int index) {
+    		if (index != 0) { 
+	    		int parentIndex = (index - 1) / NUM_CHILDREN;
+	    		if (heap[parentIndex].compareTo(heap[index]) > 0) {
+	    			T temp = heap[parentIndex];
+	    			heap[parentIndex] = heap[index];
+	    			heap[index] = temp;
+	    			insertHelper(parentIndex);
+	    		}
+    		}
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void ensureCapacity() {
+    		if (this.size >= this.heap.length) {
+			T[] newHeap = (T[]) (new Comparable[this.heap.length * 2]);
+    			for (int i = 0; i < this.heap.length; i++) {
+    				newHeap[i] = heap[i];
+    			}
+    			this.heap = newHeap;
+    		}
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
+    }
+    
+    
+    @Override
+    public void remove(T item) {
+        // TODO Auto-generated method stub
     }
 }
